a4d8145cfd073cac56b9f078b793cf83373997ed
fernandofs21 <fernando.silva@sptech.school>
2024-09-01 12:57:29 -0300

    Teste de conexão ao banco com Java

---
+++ conexao-banco/src/main/java/school/sptech/Conexao.java
@@ -0,0 +1,22 @@
+package school.sptech;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class Conexao {
+    private JdbcTemplate conexaoDoBanco;
+
+    public Conexao() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/teste"); // onde está escrito 'teste' deve ir o nome da database previamente criado.
+        dataSource.setUsername("root");
+        dataSource.setPassword("211004");
+
+        conexaoDoBanco = new JdbcTemplate(dataSource);
+    }
+
+    public JdbcTemplate getConexaoDoBanco() {
+        return conexaoDoBanco;
+    }
+}
---
+++ conexao-banco/src/main/java/school/sptech/Filme.java
@@ -0,0 +1,48 @@
+package school.sptech;
+
+public class Filme {
+    private Integer id;
+    private String nome;
+    private Integer anoLancamento;
+
+    public Filme() {}
+
+    public Filme(Integer id, String nome, Integer anoLancamento) {
+        this.id = id;
+        this.nome = nome;
+        this.anoLancamento = anoLancamento;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void setAnoLancamento(Integer anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    @Override
+    public String toString() {
+        return "\nFilme{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", anoLancamento=" + anoLancamento +
+                "}";
+    }
+}
