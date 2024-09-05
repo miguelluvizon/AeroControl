dab0aead0d415463117ca3cf2c2806bcd73a40e4
GiorgioLA <Giorgio.Antunes@sptech.school>
2024-09-04 21:36:39 -0300

    detalhes corrijidos no nuvemEC2

--- CalcComp/nuvemEC2.py
+++ CalcComp/nuvemEC2.py
@@ -6,9 +6,7 @@ def monitorar():
     porcentagemCPU = (psutil.cpu_percent(interval = 1))
     usoRAM = round(psutil.virtual_memory().used / pow(10, 9), 2)
     porcentagemRAM = psutil.virtual_memory().percent
-    totalDisco = round(psutil.disk_usage("C:\\").total / pow(10, 9), 2)
-    livreDisco = round(psutil.disk_usage("C:\\").free / pow(10, 9), 2)
-    print("Porcentagem de uso da CPU: {}%\n\nUso de memória RAM: {}GB\nPorcentagem RAM: {}%\nTotal do disco: {}%\n GB de disco livre: {}".format( porcentagemCPU, usoRAM, porcentagemRAM, totalDisco, livreDisco))
+    print("Porcentagem de uso da CPU: {}%\n\nUso de memória RAM: {}GB\nPorcentagem RAM: {}%\nTotal do disco: {}%\n GB de disco livre: {}".format( porcentagemCPU, usoRAM, porcentagemRAM))
 
     
     time.sleep(2)
