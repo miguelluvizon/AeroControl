c11f87366ebf59ba948b299d70f17ad8d8cc24d2
murilo-martinez <murilo.martinez@sptech.school>
2024-10-07 14:10:57 -0300

    [FEATURE] [EM PROGRESSO] JSON em Python

--- CalcComp/registroNuvem.py
+++ CalcComp/registroNuvem.py
@@ -4,9 +4,24 @@ import csv
 import datetime as dt
 import boto3
 import os
+import json
 
 bucket_nome = 's3-raw-aerocontrol'
 s3 = boto3.client('s3')
+
+def criar_json_se_nao_existir(nome_arquivo,dados_iniciais):
+    if not os.path.exists(nome_arquivo):
+        with open(nome_arquivo, 'w') as file:
+            json.dump(dados_iniciais, file)
+
+
+def carregar_json(nome_arquivo):
+    if not os.path.exists(nome_arquivo):
+        with open(nome_arquivo, 'r') as file:
+            json.load(file)
+    else:
+        temp = []
+        return temp
 
 def criar_csv_se_nao_existir(nome_arquivo):
     if not os.path.exists(nome_arquivo):
@@ -24,7 +39,9 @@ def monitorar():
 
     data_atual = dt.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     nome_arquivo = f'./raw_data_{data_atual}.csv'
+    nome_arquivo_json = "dados.json"
     criar_csv_se_nao_existir(nome_arquivo)
+    criar_json_se_nao_existir(nome_arquivo_json, [])
 
     while True:
         passou += 1
@@ -33,8 +50,20 @@ def monitorar():
         usoRAM = round(psutil.virtual_memory().used)
         porcentagemRAM = psutil.virtual_memory().percent
 
+        data = {
+            'cpu': porcentagemCPU,
+            'memroia RAM Bytes' : usoRAM,
+            'memoria RAM (%)': porcentagemRAM
+        }
+
         dados = [porcentagemCPU, usoRAM, porcentagemRAM]
         print(f"Porcentagem de uso da CPU: {porcentagemCPU}%\nUso de memória RAM: {usoRAM} Bytes\nPorcentagem RAM: {porcentagemRAM}%\nCiclo: {passou}")
+
+        dados_json = carregar_json(nome_arquivo_json)
+        dados_json.append(data)
+
+        with open(nome_arquivo_json, "w", encoding="utf-8") as file:
+            json.dump(dados_json, file, indent=4)
 
         with open(nome_arquivo, 'a', newline='') as csvfile:
             writer = csv.writer(csvfile, delimiter=',')
@@ -47,6 +76,7 @@ def monitorar():
 
             try:
                 s3.upload_file(nome_arquivo, bucket_nome, f'raw_data/{data_atual}.csv')
+                s3.upload_file(nome_arquivo_json, bucket_nome, f'raw_data/{nome_arquivo_json}.json')
                 print(f"Arquivo {nome_arquivo} enviado para o bucket {bucket_nome}.")
             except Exception as e:
                 print(f"Erro ao enviar o arquivo para o S3: {str(e)}")
@@ -57,7 +87,7 @@ def monitorar():
             data_atual = dt.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
             nome_arquivo = f'./raw_data_{data_atual}.csv'
             criar_csv_se_nao_existir(nome_arquivo)
-            print(f"Novo arquivo CSV {nome_arquivo} criado após atingir 20.000 dados.")
+            print(f"Novo arquivo CSV {nome_arquivo} criado após atingir 5.000 dados.")
 
         time.sleep(tempo)
 
