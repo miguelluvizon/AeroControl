9b3b5d707ec8bb37b1dbc6d4b9e2cea8c6339060
Arthurhzr <arthur.rocha@sptech.school>
2024-10-14 01:16:39 -0300

    Ajustes no Java

--- ETL Java climaTempo/tratamento-aerocontrol/src/main/java/school/sptech/Forecast.java
+++ ETL Java climaTempo/tratamento-aerocontrol/src/main/java/school/sptech/Forecast.java
@@ -1,7 +1,9 @@
 package school.sptech;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Forecast {
 
     @JsonProperty("date")
--- ETL Java climaTempo/tratamento-aerocontrol/src/main/java/school/sptech/Mapper.java
+++ ETL Java climaTempo/tratamento-aerocontrol/src/main/java/school/sptech/Mapper.java
@@ -1,5 +1,6 @@
 package school.sptech;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
@@ -9,7 +10,8 @@ import java.util.List;
 public class Mapper {
     public List<Forecast> map(InputStream inputStream) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
-        return mapper.readValue(inputStream, new TypeReference<List<Forecast>>() {
-        });
+        JsonNode rootNode = mapper.readTree(inputStream);
+        JsonNode forecastNode = rootNode.get(0).get("forecast");
+        return mapper.convertValue(forecastNode, new com.fasterxml.jackson.core.type.TypeReference<List<Forecast>>() {});
     }
 }
