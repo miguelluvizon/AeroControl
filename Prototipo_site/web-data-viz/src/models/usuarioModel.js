1f7cd382eef722d41a0c889f8d3aa50a4547fef1
murilo-martinez <murilo.martinez@sptech.school>
2024-10-19 20:27:23 -0300

    Arrumando o model do Login

--- Prototipo_site/web-data-viz/src/models/usuarioModel.js
+++ Prototipo_site/web-data-viz/src/models/usuarioModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config")
 function autenticar(cpf, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", cpf, senha)
     var instrucaoSql = `
-        SELECT idUsuario, nome, email, senha, cpf, cargo FROM Usuario WHERE cpf = '${cpf}' AND senha = '${senha}';
+        SELECT cpf, nomeUsuario, email, senha, cargo, fkAeroporto FROM Usuario WHERE cpf = '${cpf}' AND senha = '${senha}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
