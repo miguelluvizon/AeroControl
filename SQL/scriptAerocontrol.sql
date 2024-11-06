b48631cb4b34ee8e886f52301bbc68d97630720c
murilo-martinez <murilo.martinez@sptech.school>
2024-11-06 17:31:14 -0300

    Arrumando o script do Banco de Dados

--- SQL/scriptAerocontrol.sql
+++ SQL/scriptAerocontrol.sql
@@ -70,49 +70,18 @@ INSERT INTO Usuario VALUES
 	('25107632415' , 'Murilo Martinez' , 'murilo@gmail.com' , 'murilo123', 'Analista de Dados' , '43210987654321');
 
 INSERT INTO Setor VALUES
-	(1, '11223344556677', 'Despache de Voo'),
-	(1, '43210987654321', 'Despache de Voo'),
-	(2, '11223344556677', 'Torre de Controle'),
-	(1, '12345678901234', 'Despache de Voo'),
-	(2, '12345678901234', 'Torre de Controle'),
-	(3, '43210987654321', 'Centro de Meteorologia'),
-	(3, '11223344556677', 'Centro de Meteorologia'),
-	(2, '43210987654321', 'Torre de Controle'),
-	(3, '12345678901234', 'Centro de Meteorologia');
+	(1, 'Despache de Voo','11223344556677'),
+	(2, 'Despache de Voo','43210987654321'),
+	(3, 'Torre de Controle', '11223344556677'),
+	(4, 'Despache de Voo', '12345678901234'),
+	(5, 'Torre de Controle', '12345678901234'),
+	(6, 'Centro de Meteorologia', '43210987654321'),
+	(7, 'Centro de Meteorologia', '11223344556677'),
+	(8, 'Torre de Controle', '43210987654321'),
+	(9, 'Centro de Meteorologia', '12345678901234');
 
 INSERT INTO Computador VALUES
-	(1, '12345678901234', '1', 'C1DP', 'i3', '8', 1),
-	(1, '12345678901234', '2', 'C1CN', 'i3', '8', 3),
-	(2, '12345678901234', '2', 'C2CN', 'i3', '8', 3),
-	(1, '11223344556677', '1', 'CO1D', 'i5', '16', 3),
-	(2, '11223344556677', '1', 'CO2D', 'i5', '16', 2),
-	(3, '12345678901234', '2', 'C3CN', 'i3', '8', 1),
-	(3, '11223344556677', '1', 'CO3D', 'i5', '8', 1),
-	(4, '11223344556677', '1', 'CO4D', 'i5', '8', 1),
-	(2, '12345678901234', '1', 'C2DP', 'i3', '8', 1),
-	(1, '11223344556677', '2', 'CO1C', 'i5', '8', 1),
-	(1, '43210987654321', '1', 'GCD1', 'i9', '8', 1),
-	(2, '43210987654321', '1', 'GCD2', 'i9', '8', 1),
-	(3, '43210987654321', '1', 'GCD3', 'i9', '8', 1),
-	(1, '43210987654321', '2', 'GCC1', 'i9', '8', 1),
-	(2, '43210987654321', '2', 'GCC2', 'i9', '8', 1),
-	(3, '43210987654321', '2', 'GCC3', 'i9', '8', 1),
-	(2, '11223344556677', '2', 'CO2C', 'i5', '8', 1),
-	(3, '12345678901234', '1', 'C3DP', 'i3', '16', 2),
-	(4, '12345678901234', '1', 'C4DP', 'i3', '16', 2),
-	(1, '43210987654321', '3', 'GCM1', 'i9', '16', 2),
-	(1, '12345678901234', '3', 'C1MT', 'i3', '16', 2),
-	(2, '12345678901234', '3', 'C2MT', 'i3', '16', 2),
-	(3, '12345678901234', '3', 'C3MT', 'i3', '16', 2),
-	(4, '12345678901234', '3', 'C4MT', 'i3', '16', 2),
-	(2, '43210987654321', '3', 'GCM2', 'i9', '16', 2),
-	(3, '43210987654321', '3', 'GCM3', 'i9', '16', 2),
-	(4, '43210987654321', '3', 'GCM4', 'i9', '16', 2),
-	(3, '11223344556677', '2', 'CO3C', 'i5', '16', 2),
-	(1, '11223344556677', '3', 'CO1M', 'i5', '16', 2),
-	(2, '11223344556677', '3', 'CO2M', 'i5', '16', 2),
-	(3, '11223344556677', '3', 'CO3M', 'i5', '16', 2),
-	(4, '11223344556677', '3', 'CO4M', 'i5', '16', 2);
+	(1, 'C1DP', 'i3', '8' ,'12345678901','11223344556677', 1);
     
 SELECT * FROM Aeroporto;
 SELECT * FROM Setor;    
@@ -121,19 +90,4 @@ SELECT * FROM Computador;
 SELECT * FROM DadoComputador;
 SELECT * FROM Alerta;
 
-INSERT INTO Aeroporto VALUES
-	(1, 'Congonhas', '3641-001', 1);
-
-INSERT INTO Computador VALUES
-	(2, 'nb-martinez', 'i5-1334', '16GB', '1TB', 'Comunicação com Pilotos', 1);
-    
-SELECT * FROM DadoComputador;
-    
-SELECT * FROM Aeroporto;
-
-SELECT * FROM usuario;
-SELECT * FROM Aeroporto;    
-SELECT * FROM Usuario;  
-SELECT * FROM Computador;
-SELECT * FROM DadoComputador;
-
+SELECT cnpj, nomeAeroporto FROM Aeroporto ORDER BY nomeAeroporto;
