f1161d3d7673d4511ea13a681d94e32489427602
murilo-martinez <murilo.martinez@sptech.school>
2024-11-19 23:03:40 -0300

    Adicionando as rotas e fazendo a dash parar de ser estática [Martinez]

--- Prototipo_site/web-data-viz/app.js
+++ Prototipo_site/web-data-viz/app.js
@@ -18,6 +18,7 @@ var app = express();
 var usuarioRouter = require("./src/routes/usuarios");
 var empresasRouter = require("./src/routes/empresas");
 var suporteRouter = require("./src/routes/suporte");
+var comparacaoRouter = require("./src/routes/comparacao");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -28,6 +29,7 @@ app.use(cors());
 app.use("/usuarios", usuarioRouter);
 app.use("/empresas", empresasRouter);
 app.use("/suporte", suporteRouter);
+app.use("/comparacao", comparacaoRouter);
 
 app.listen(PORTA_APP, function () {
     console.log(`
---
+++ Prototipo_site/web-data-viz/src/models/comparacaoModel.js
@@ -0,0 +1,93 @@
+var database = require("../database/config")
+
+function totalAlertas() {
+    const instrucao = `SELECT count(idAlerta) as qtdAlertas FROM Alerta;`
+    return database.executar(instrucao)
+}
+
+function totalAletasSetor(setor) {
+    const instrucao = `
+        SELECT 
+            s.nomeSetor AS setor,
+            COUNT(a.idAlerta) AS quantidade_alertas
+        FROM 
+            Alerta a
+        JOIN 
+            DadoComputador dc ON a.fkDadoComputador = dc.idDado
+        JOIN 
+            Computador c ON dc.fkComputador = c.idComputador
+        JOIN 
+            Setor s ON c.fkSetor = s.idSetor
+        WHERE 
+            s.idSetor = ${setor}
+        GROUP BY 
+            s.nomeSetor
+        ORDER BY 
+            s.nomeSetor;
+    `
+
+    return database.executar(instrucao)
+}
+
+function exibirGraficoEleMesmo(setor){
+    const instrucao = `
+        SELECT 
+            s.nomeSetor AS setor,
+            SUM(CASE WHEN DAY(a.dataAlerta) <= 7 THEN 1 ELSE 0 END) AS semana_1,
+            SUM(CASE WHEN DAY(a.dataAlerta) BETWEEN 8 AND 14 THEN 1 ELSE 0 END) AS semana_2,
+            SUM(CASE WHEN DAY(a.dataAlerta) BETWEEN 15 AND 21 THEN 1 ELSE 0 END) AS semana_3,
+            SUM(CASE WHEN DAY(a.dataAlerta) >= 22 THEN 1 ELSE 0 END) AS semana_4
+        FROM 
+            Alerta a
+        JOIN 
+            DadoComputador dc ON a.fkDadoComputador = dc.idDado
+        JOIN 
+            Computador c ON dc.fkComputador = c.idComputador
+        JOIN 
+            Setor s ON c.fkSetor = s.idSetor
+        WHERE 
+            MONTH(a.dataAlerta) = MONTH(CURRENT_DATE())
+            AND YEAR(a.dataAlerta) = YEAR(CURRENT_DATE())
+            AND s.idSetor = ${setor}
+        GROUP BY 
+            s.nomeSetor
+        ORDER BY 
+            s.nomeSetor;
+    `
+    return database.executar(instrucao)
+}
+
+function exibirGraficoSetores(){
+    const instrucao = `
+        SELECT 
+            s.nomeSetor AS setor,
+            SUM(CASE WHEN DAY(a.dataAlerta) <= 7 THEN 1 ELSE 0 END) AS semana_1,
+            SUM(CASE WHEN DAY(a.dataAlerta) BETWEEN 8 AND 14 THEN 1 ELSE 0 END) AS semana_2,
+            SUM(CASE WHEN DAY(a.dataAlerta) BETWEEN 15 AND 21 THEN 1 ELSE 0 END) AS semana_3,
+            SUM(CASE WHEN DAY(a.dataAlerta) >= 22 THEN 1 ELSE 0 END) AS semana_4
+        FROM 
+            Alerta a
+        JOIN 
+            DadoComputador dc ON a.fkDadoComputador = dc.idDado
+        JOIN 
+            Computador c ON dc.fkComputador = c.idComputador
+        JOIN 
+            Setor s ON c.fkSetor = s.idSetor
+        WHERE 
+            MONTH(a.dataAlerta) = MONTH(CURRENT_DATE())
+            AND YEAR(a.dataAlerta) = YEAR(CURRENT_DATE())
+        GROUP BY 
+            s.nomeSetor
+        ORDER BY 
+            s.nomeSetor;
+    `
+
+    return database.executar(instrucao)
+}
+
+module.exports = {
+    totalAlertas,
+    totalAletasSetor,
+    exibirGraficoEleMesmo,
+    exibirGraficoSetores
+}
